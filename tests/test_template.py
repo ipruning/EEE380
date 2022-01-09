657f00bca5b1432bc2a12cb0330890fb26b47d43
Alex <30626291+Spehhhhh@users.noreply.github.com>
2022-01-09 12:57:04 +0000

    init tests

---
+++ tests/test_template.py
@@ -0,0 +1,32 @@
+import os
+import unittest
+from loguru import logger
+
+
+logger.add(
+    "logs/" + str(os.path.basename(__file__)) + ".log",
+    level="DEBUG",
+    format="{time:YYYY-MM-DD :mm:ss} - {level} - {file} - {line} - {message}",
+    rotation="10 MB",
+)
+logger.info("Start Log")
+
+
+class TestTemplate(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    @logger.catch
+    def test_1(self):
+        pass
