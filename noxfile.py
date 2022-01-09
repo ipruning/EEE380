f7eea214bed0004a79380f43994481671da94bbe
Alex <30626291+Spehhhhh@users.noreply.github.com>
2022-01-09 12:58:33 +0000

    add nox

---
+++ noxfile.py
@@ -0,0 +1,58 @@
+import tempfile
+
+import nox
+
+src = "fatigue-detection", "tests"
+
+
+# from nox_poetry import session
+# session.install("black")
+def install_with_constraints(session, *args, **kwargs):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--without-hashes",
+            "--format=requirements.txt",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install(f"--constraint={requirements.name}", *args, **kwargs)
+
+
+@nox.session
+def formatting(session):
+    install_with_constraints(session, "black", "isort")
+    session.run("black", ".")
+    session.run("isort", ".")
+
+
+@nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11", "3.12"])
+def testing(session):
+    install_with_constraints(session, "pytest", "pytest-cov", "coverage[toml]", "loguru")
+    session.run("coverage", "run", "-m", "pytest")
+    session.run("coverage", "report")
+
+
+@nox.session
+def linting(session):
+    install_with_constraints(session, "flake8")
+    session.run("flake8", ".", "--exit-zero")
+
+
+@nox.session
+def linting_pylint(session):
+    install_with_constraints(session, "pylint")
+    session.run("pylint", "--fail-under=9", *src)
+
+
+@nox.session
+def typing(session):
+    install_with_constraints(session, "mypy")
+    session.run("mypy", *src)
+
+
+@nox.session
+def building(session):
+    pass
