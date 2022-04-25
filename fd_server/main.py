ed9044155545ae9e121441be37953dc1affa3f83
Alex <30626291+Spehhhhh@users.noreply.github.com>
2022-04-25 12:16:01 +0100

    refactor: fatigue_index

--- fd_server/main.py
+++ fd_server/main.py
@@ -36,18 +36,17 @@ def callback_alarm(context: telegram.ext.CallbackContext) -> None:
     url = "http://127.0.0.1:5000/api/v1/get_sensor_data"
     resp = requests.get(url=url)
     sensor_data = resp.json()
-    print(sensor_data)
     my_message = f"Fatigue Value: {str(sensor_data[0])}"
     context.bot.send_message(chat_id=context.job.context, text=my_message)
 
 
 def callback_start_m(update: telegram.Update, context: telegram.ext.CallbackContext) -> None:
-    context.bot.send_message(chat_id=update.message.chat_id, text="Setting a timer!")
-    context.job_queue.run_repeating(callback_alarm, 1, context=update.message.chat_id)
+    context.bot.send_message(chat_id=update.message.chat_id, text="Prepare for monitoring!")
+    context.job_queue.run_repeating(callback_alarm, 3, context=update.message.chat_id)
 
 
 def callback_stop_m(update: telegram.Update, context: telegram.ext.CallbackContext) -> None:
-    context.bot.send_message(chat_id=update.message.chat_id, text="Stopping the timer!")
+    context.bot.send_message(chat_id=update.message.chat_id, text="Stopping the monitoring!")
     context.job_queue.stop()
 
 
@@ -56,8 +55,8 @@ app = flask.Flask(__name__)
 
 @app.route("/api/v1/get_sensor_data", methods=["GET", "POST"])
 def draw_stone():
-    d_hum, d_temp = round(random.uniform(0, 1), 4), round(random.uniform(0, 1), 4)
-    data = [d_temp, d_hum]
+    fatigue_index, d_hum = round(random.uniform(0, 1), 4), round(random.uniform(0, 1), 4)
+    data = [fatigue_index, d_hum]
     return flask.Response(json.dumps(data), mimetype="application/json")
 
 
